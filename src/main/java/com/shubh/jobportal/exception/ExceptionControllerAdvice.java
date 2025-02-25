6b2d6aea2da02fbb981a7523a1fdeb43ba35604f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-25 21:48:50 +0530

    adding Exception Controller Advice

---
+++ src/main/java/com/shubh/jobportal/exception/ExceptionControllerAdvice.java
@@ -0,0 +1,43 @@
+package com.shubh.jobportal.exception;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorInfo> generalException(Exception ex){
+        ErrorInfo errorInfo = new ErrorInfo(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
+        LocalDateTime.now());
+
+        return new ResponseEntity<ErrorInfo>(errorInfo, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    public ResponseEntity<ErrorInfo> validatorExceptionHandler(Exception ex){
+        String message = "";
+        if (ex instanceof MethodArgumentNotValidException mave) {
+            message = mave.getAllErrors().stream().map((ObjectError::getDefaultMessage)).collect(Collectors.joining(", "));
+        }else{
+            var cve = (ConstraintViolationException) ex;
+            message = cve.getConstraintViolations().stream()
+            .map(ConstraintViolation::getMessage).collect(Collectors.joining(", "));
+        }
+
+        ErrorInfo errorInfo = new ErrorInfo(message, HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now());
+
+        return new ResponseEntity<ErrorInfo>(errorInfo, HttpStatus.BAD_REQUEST);
+    }
+}
