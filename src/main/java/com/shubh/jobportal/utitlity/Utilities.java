180512a0842be526cb5a9053a8548b8395208647
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-25 22:49:10 +0530

    adding static method call for Utilities

--- src/main/java/com/shubh/jobportal/utitlity/Utilities.java
+++ src/main/java/com/shubh/jobportal/utitlity/Utilities.java
@@ -5,26 +5,39 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Component;
 
 import com.shubh.jobportal.entity.Sequence;
+import com.shubh.jobportal.exception.JobPortalException;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Component
 public class Utilities {
-    
+
     private final MongoOperations mongoOperations;
+    private static MongoOperations staticMongoOperations; // Static field
 
-    public Long getNextSequence(String key) {
+    @PostConstruct
+    public void init() {
+        staticMongoOperations = mongoOperations; // Assign instance to static field
+    }
+
+    public static Long getNextSequence(String key) throws JobPortalException {
+        if (staticMongoOperations == null) {
+            throw new IllegalStateException("MongoOperations is not initialized by Spring.");
+        }
+
         Query query = new Query(Criteria.where("_id").is(key));
-
         Update update = new Update().inc("seq", 1);
         FindAndModifyOptions options = new FindAndModifyOptions().returnNew(true);
 
-        Sequence seq = mongoOperations.findAndModify(query, update, options, Sequence.class);
+        Sequence seq = staticMongoOperations.findAndModify(query, update, options, Sequence.class);
 
         if (seq == null) {
-            throw new IllegalStateException("Unable to get sequence for key: " + key);
+            throw new JobPortalException("Unable to get sequence for key: " + key);
         }
 
         return seq.getSeq();
