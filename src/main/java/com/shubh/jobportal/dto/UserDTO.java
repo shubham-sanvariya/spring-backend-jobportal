72e308a00c07df6c06d196880f5608ede318bb73
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-25 19:05:50 +0530

    adding validation  on fields

--- src/main/java/com/shubh/jobportal/dto/UserDTO.java
+++ src/main/java/com/shubh/jobportal/dto/UserDTO.java
@@ -2,6 +2,7 @@ package com.shubh.jobportal.dto;
 
 import com.shubh.jobportal.entity.User;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserDTO {
     private String id;
+    @NotBlank(message = "Name is null or blank")
     private String name;
+    @NotBlank(message = "Email is null or blank")
     private String email;
+    @NotBlank(message = "password is null or blank")
     private String password;
+
     private AccountType accountType;
 
     public User toEntity() {
