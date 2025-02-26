f4c8762b72803c68ccd53c35205137c19d789e3a
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-26 15:06:16 +0530

    adding security filter chain method

--- src/main/java/com/shubh/jobportal/config/SecurityConfig.java
+++ src/main/java/com/shubh/jobportal/config/SecurityConfig.java
@@ -2,11 +2,22 @@ package com.shubh.jobportal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+       return http
+       .csrf(req -> req.disable())
+       .authorizeHttpRequests(
+        auth -> auth.requestMatchers("/*")
+        .permitAll()).build();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
