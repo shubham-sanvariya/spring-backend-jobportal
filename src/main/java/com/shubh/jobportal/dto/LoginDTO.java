6a0b6fa75a3ac856203d930b97b2a6da5d3acb05
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-26 15:45:45 +0530

    adding validation on login dto

--- src/main/java/com/shubh/jobportal/dto/LoginDTO.java
+++ src/main/java/com/shubh/jobportal/dto/LoginDTO.java
@@ -1,5 +1,8 @@
 package com.shubh.jobportal.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginDTO {
+    @NotBlank(message = "{user.email.absent}")
+    @Email(message = "{user.email.invalid}")
     private String email;
+    @NotBlank(message = "{user.password.absent}")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,15}$", message = "{user.password.invalid}")
     private String password;
 }
