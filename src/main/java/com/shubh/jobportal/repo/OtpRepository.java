83acdba18ad1a3dbba275768ad2b8594af9f44d3
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-27 21:38:16 +0530

    addind find by expiration time otps method in otp repo

--- src/main/java/com/shubh/jobportal/repo/OtpRepository.java
+++ src/main/java/com/shubh/jobportal/repo/OtpRepository.java
@@ -3,7 +3,10 @@ package com.shubh.jobportal.repo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.shubh.jobportal.entity.Otp;
+import java.util.List;
+import java.time.LocalDateTime;
+
 
 public interface OtpRepository extends MongoRepository<Otp,String>{
-    
+    List<Otp> findByCreationTimeBefore(LocalDateTime expiryTime);
 }
