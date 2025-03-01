6288bdfceb70e87131d9119f02ca139791af9e11
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-01 16:11:31 +0530

    adding getProfileById mapping in profile controller

--- src/main/java/com/shubh/jobportal/controller/ProfileController.java
+++ src/main/java/com/shubh/jobportal/controller/ProfileController.java
@@ -1,14 +1,31 @@
 package com.shubh.jobportal.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shubh.jobportal.dto.ProfileDTO;
+import com.shubh.jobportal.exception.JobPortalException;
+import com.shubh.jobportal.service.ProfileService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @CrossOrigin
 @Validated
+@RequiredArgsConstructor
 @RequestMapping("/profile")
 public class ProfileController {
     
+    private final ProfileService profileService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProfileDTO> getProfileById(@PathVariable Long id) throws JobPortalException{
+        return new ResponseEntity<ProfileDTO>(profileService.getProfile(id),HttpStatus.OK);
+    }
 }
