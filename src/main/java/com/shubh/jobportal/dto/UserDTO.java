8b180e1286625b7e2772e5c814ddccccd2ad569e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-01 15:31:27 +0530

    adding profileId attribute in user & it's dto

--- src/main/java/com/shubh/jobportal/dto/UserDTO.java
+++ src/main/java/com/shubh/jobportal/dto/UserDTO.java
@@ -24,8 +24,9 @@ public class UserDTO {
     private String password;
 
     private AccountType accountType;
+    private Long profileId;
 
     public User toEntity() {
-        return new User(this.id, this.name, this.email, this.password, this.accountType);
+        return new User(this.id, this.name, this.email, this.password, this.accountType,this.profileId);
     }
 }
