e7c4d1297134f2a8a7672b6a3ecc0315d09f6f72
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-06 16:57:33 +0530

    adding getAll jobs req mapping

--- src/main/java/com/shubh/jobportal/controller/JobController.java
+++ src/main/java/com/shubh/jobportal/controller/JobController.java
@@ -1,9 +1,12 @@
 package com.shubh.jobportal.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +31,10 @@ public class JobController {
     @PostMapping("/post")
     public ResponseEntity<JobDTO> postJob(@RequestBody @Valid JobDTO jobDTO) throws JobPortalException{
         return new ResponseEntity<JobDTO>(jobService.postJob(jobDTO),HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<JobDTO>> getAllJobs() throws JobPortalException{
+        return new ResponseEntity<List<JobDTO>>(jobService.getAllJobs(),HttpStatus.OK);
     }
 }
