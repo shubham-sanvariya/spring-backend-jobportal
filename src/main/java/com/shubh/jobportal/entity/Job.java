e512b75f0c3b17a0eba47622fee12228975bd580
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-06 16:11:43 +0530

    adding Job entity

---
+++ src/main/java/com/shubh/jobportal/entity/Job.java
@@ -0,0 +1,43 @@
+ package com.shubh.jobportal.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.shubh.jobportal.dto.Applicant;
+import com.shubh.jobportal.dto.JobDTO;
+import com.shubh.jobportal.enums.JobStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "jobs")
+public class Job {
+
+    @Id
+    private Long id;
+    private String  jobTitle;
+    private String company;
+    private List<Applicant> applicants;
+    private String about;
+    private String experience;
+    private String jobType;
+    private String location;
+    private Long packageOffered;
+    private LocalDateTime postTime;
+    private String description;
+    private List<String> skillRequired;
+    private JobStatus jobStatus;
+
+    public JobDTO toEntity() {
+        return new JobDTO(this.id, this.jobTitle, this.company, this.applicants, this.about, this.experience, this.jobType,
+                this.location, this.packageOffered, this.postTime, this.description, this.skillRequired,
+                this.jobStatus);
+    }
+}
