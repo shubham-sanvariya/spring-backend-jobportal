a7aa8dc29202183c5d97cf58f00b18228386913b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-09 15:13:57 +0530

    adding findByPostedBy method in job repo

--- src/main/java/com/shubh/jobportal/repo/JobRepository.java
+++ src/main/java/com/shubh/jobportal/repo/JobRepository.java
@@ -3,7 +3,9 @@ package com.shubh.jobportal.repo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.shubh.jobportal.entity.Job;
+import java.util.List;
+
 
 public interface JobRepository extends MongoRepository<Job,Long>{
-    
+    List<Job> findByPostedBy(Long postedBy);
 }
