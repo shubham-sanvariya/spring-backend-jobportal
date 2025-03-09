730740bc0033502bfa99bedf89df4602832da7f1
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-09 16:43:06 +0530

    adding updateJobApplication req mapping

--- src/main/java/com/shubh/jobportal/controller/JobController.java
+++ src/main/java/com/shubh/jobportal/controller/JobController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shubh.jobportal.dto.ApplicantDTO;
+import com.shubh.jobportal.dto.ApplicationDTO;
 import com.shubh.jobportal.dto.JobDTO;
 import com.shubh.jobportal.service.JobService;
 
@@ -41,12 +43,18 @@ public class JobController {
 
     @GetMapping("/{id}")
     public ResponseEntity<JobDTO> getJobById(@PathVariable Long id){
-        return new ResponseEntity<JobDTO>(jobService.getJobById(id),HttpStatus.OK);
+        return new ResponseEntity<JobDTO>(jobService.getJobDTOById(id),HttpStatus.OK);
     }
 
     @PostMapping("/apply/{id}")
     public ResponseEntity<String> applyJob(@PathVariable Long id, @RequestBody @Valid ApplicantDTO applicantDTO){
         jobService.applyJob(id,applicantDTO);
         return new ResponseEntity<String>("Applied Successfully",HttpStatus.OK);
+    }
+
+    @PutMapping("/applicant/update")
+    public ResponseEntity<String> updateJobApplicant(@RequestBody @Valid ApplicationDTO applicationDTO){
+        jobService.changeApplicationStatus(applicationDTO);
+        return new ResponseEntity<String>("Job Application Status Updated",HttpStatus.OK);
     }
 }
