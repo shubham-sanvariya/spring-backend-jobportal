d6dc462cd22a00ef036c908e31efeb8ea5892770
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-19 21:57:20 +0530

    adding updateUserName req mapping

--- src/main/java/com/shubh/jobportal/controller/UserController.java
+++ src/main/java/com/shubh/jobportal/controller/UserController.java
@@ -5,16 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shubh.jobportal.dto.LoginDTO;
 import com.shubh.jobportal.dto.UserDTO;
 import com.shubh.jobportal.service.UserService;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
@@ -57,5 +58,10 @@ public class UserController {
          {
         userService.changePassword(resetDto);
         return new ResponseEntity<>("Password Changed Successfully.", HttpStatus.OK);
+    }
+
+    @PatchMapping("/update/{id}/username")
+    public ResponseEntity<UserDTO> updateUserName(@PathVariable Long id, @RequestParam String userName){
+        return new ResponseEntity<UserDTO>(userService.updateUserName(id, userName),HttpStatus.CREATED);
     }
 }
