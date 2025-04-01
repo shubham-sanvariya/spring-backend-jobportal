0f400e3deb6d9ccb0a0b2c4275d2772873295834
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-01 11:25:24 +0530

    add pagination support to JobRepository for job status queries

--- src/main/java/com/shubh/jobportal/repo/JobRepository.java
+++ src/main/java/com/shubh/jobportal/repo/JobRepository.java
@@ -1,11 +1,17 @@
 package com.shubh.jobportal.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.shubh.jobportal.entity.Job;
 import java.util.List;
+import com.shubh.jobportal.enums.JobStatus;
+
 
 
 public interface JobRepository extends MongoRepository<Job,Long>{
     List<Job> findByPostedBy(Long postedBy);
+
+    Page<Job> findByJobStatus(JobStatus jobStatus, Pageable pageable);
 }
