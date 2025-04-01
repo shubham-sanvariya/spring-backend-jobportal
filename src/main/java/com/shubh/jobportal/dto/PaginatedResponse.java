d1111aa09c661b10ab40703b866c29d85b872f52
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-01 15:03:06 +0530

    add PaginatedResponse class to support pagination in API responses

---
+++ src/main/java/com/shubh/jobportal/dto/PaginatedResponse.java
@@ -0,0 +1,30 @@
+package com.shubh.jobportal.dto;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PaginatedResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+
+    public static <T> PaginatedResponse<T> fromPage(Page<T> page){
+        return new PaginatedResponse<>(
+            page.getContent(),
+            page.getNumber(),
+            page.getSize(),
+            page.getTotalElements(),
+            page.getTotalPages(),
+            page.isLast()
+        );
+    }
+}
