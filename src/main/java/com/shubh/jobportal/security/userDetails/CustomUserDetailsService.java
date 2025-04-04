d29b17cf4211ff934f7ac3b76a4eb690084b5e2e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-05 01:14:57 +0530

    Add CustomUserDetailsService for loading user details by username or email

---
+++ src/main/java/com/shubh/jobportal/security/userDetails/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.shubh.jobportal.security.userDetails;
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.shubh.jobportal.entity.User;
+import com.shubh.jobportal.repo.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+    
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        Optional<User> optionalUser = isEmail(usernameOrEmail) 
+        ? userRepository.findByEmail(usernameOrEmail)
+        : userRepository.findByName(usernameOrEmail);
+
+        User user = optionalUser
+                .orElseThrow(() -> new UsernameNotFoundException("user not found by: " + usernameOrEmail));
+
+        return CustomUserDetails.buildUserDetails(user);
+    }
+
+    public boolean isEmail(String input) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+
+        return input.matches(emailRegex);
+    }
+}
