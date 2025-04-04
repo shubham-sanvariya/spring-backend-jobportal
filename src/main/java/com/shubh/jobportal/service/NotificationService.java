b90fa5ce421461cc3a83a493dedb1510689d4c00
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-04 08:41:37 +0530

    implement sendNotification method in NotificationServiceImpl to handle notification creation and saving

--- src/main/java/com/shubh/jobportal/service/NotificationService.java
+++ src/main/java/com/shubh/jobportal/service/NotificationService.java
@@ -1,5 +1,7 @@
 package com.shubh.jobportal.service;
 
+import com.shubh.jobportal.dto.NotificationDTO;
+
 public interface NotificationService {
-    
+    void sendNotification(NotificationDTO notificationDTO);
 }
--- src/main/java/com/shubh/jobportal/service/NotificationServiceImpl.java
+++ src/main/java/com/shubh/jobportal/service/NotificationServiceImpl.java
@@ -1,8 +1,12 @@
 package com.shubh.jobportal.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
+import com.shubh.jobportal.dto.NotificationDTO;
 import com.shubh.jobportal.repo.NotificationRepository;
+import com.shubh.jobportal.utitlity.Utilities;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +16,12 @@ public class NotificationServiceImpl implements NotificationService{
 
     private final NotificationRepository repository;
 
-    
+    @Override
+    public void sendNotification(NotificationDTO notificationDTO) {
+        notificationDTO.setId(Utilities.getNextSequence("notifications"));
+        notificationDTO.setTimeStamp(LocalDateTime.now());
+        repository.save(notificationDTO.toEntity());
+    }
+
+     
 }
