b40d9ab98630a5cf59fe495be212dedbf2f133d1
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-05 01:14:44 +0530

    Add buildUserDetails method to CustomUserDetails for creating user details from User entity

--- src/main/java/com/shubh/jobportal/security/userDetails/CustomUserDetails.java
+++ src/main/java/com/shubh/jobportal/security/userDetails/CustomUserDetails.java
@@ -1,11 +1,15 @@
 package com.shubh.jobportal.security.userDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.shubh.jobportal.dto.AccountType;
+import com.shubh.jobportal.entity.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,6 +25,19 @@ public class CustomUserDetails implements UserDetails{
     private String password;
     private AccountType accountType;
     private Collection<GrantedAuthority> authorities;
+
+    public static CustomUserDetails buildUserDetails(User user){
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(user.getAccountType().name()));
+
+        return new CustomUserDetails(
+            user.getId(),
+            user.getEmail(),
+            user.getPassword(),
+            user.getAccountType(),
+            grantedAuthorities
+        );
+    }
     
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
