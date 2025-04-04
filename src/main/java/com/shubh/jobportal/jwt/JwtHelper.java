36fa4a00972f2b32f69413b365493218c516b4cd
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-05 00:41:53 +0530

    Refactor JwtHelper methods for improved readability and consistency in token handling

--- src/main/java/com/shubh/jobportal/jwt/JwtHelper.java
+++ src/main/java/com/shubh/jobportal/jwt/JwtHelper.java
@@ -1,13 +1,19 @@
 package com.shubh.jobportal.jwt;
 
+import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
+
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtHelper {
@@ -20,21 +26,21 @@ public class JwtHelper {
 
     public Claims getAllClaimsFromToken(String token) {
         return Jwts.parserBuilder()
-                    .setSigningKey(SECRET_KEY.getBytes())
-                    .build().parseClaimsJws(token)
-                    .getBody();
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build().parseClaimsJws(token)
+                .getBody();
     }
 
-    public <T> T getClaimsFromToken(String token, Function<Claims, T> claimsResolver){
+    public <T> T getClaimsFromToken(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
 
-    public String getUsernameOrEmailFromToken(String token){
+    public String getUsernameOrEmailFromToken(String token) {
         return getClaimsFromToken(token, Claims::getSubject);
     }
 
-    public Date getExpirationDateFromToken(String token){
-        return getClaimsFromToken(token,Claims::getExpiration);
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimsFromToken(token, Claims::getExpiration);
     }
 }
