ce56f09dd380f1e42fbd212ab7bbf8b9eb98ddd6
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-04 08:59:07 +0530

    Add getUnreadNotification method to NotificationService and implement it in NotificationServiceImpl for retrieving unread notifications

--- src/main/java/com/shubh/jobportal/service/NotificationService.java
+++ src/main/java/com/shubh/jobportal/service/NotificationService.java
@@ -1,7 +1,12 @@
 package com.shubh.jobportal.service;
 
+import java.util.List;
+
 import com.shubh.jobportal.dto.NotificationDTO;
+import com.shubh.jobportal.entity.Notification;
 
 public interface NotificationService {
     void sendNotification(NotificationDTO notificationDTO);
+
+    List<Notification> getUnreadNotification(Long userId);
 }
--- src/main/java/com/shubh/jobportal/service/NotificationServiceImpl.java
+++ src/main/java/com/shubh/jobportal/service/NotificationServiceImpl.java
@@ -1,10 +1,13 @@
 package com.shubh.jobportal.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.shubh.jobportal.dto.NotificationDTO;
+import com.shubh.jobportal.entity.Notification;
+import com.shubh.jobportal.enums.NotificationStatus;
 import com.shubh.jobportal.repo.NotificationRepository;
 import com.shubh.jobportal.utitlity.Utilities;
 
@@ -20,7 +23,13 @@ public class NotificationServiceImpl implements NotificationService{
     public void sendNotification(NotificationDTO notificationDTO) {
         notificationDTO.setId(Utilities.getNextSequence("notifications"));
         notificationDTO.setTimeStamp(LocalDateTime.now());
+        notificationDTO.setStatus(NotificationStatus.UNREAD);
         repository.save(notificationDTO.toEntity());
+    }
+
+    @Override
+    public List<Notification> getUnreadNotification(Long userId) {
+        return repository.findByUserIdAndStatus(userId,NotificationStatus.UNREAD);
     }
 
      
