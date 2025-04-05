d8c093fdd31d02feaf59279038b60e222d44c8b5
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-05 23:33:11 +0530

    Add AuthenticationManager bean and refactor CSRF configuration in SecurityConfig for improved security management

--- src/main/java/com/shubh/jobportal/config/SecurityConfig.java
+++ src/main/java/com/shubh/jobportal/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.shubh.jobportal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,7 +16,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
        return http
-       .csrf(req -> req.disable())
+       .csrf(CsrfConfigurer::disable)
        .authorizeHttpRequests(
         auth -> auth.requestMatchers("/**")
         .permitAll()).build();
@@ -22,5 +25,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception{
+        return builder.getAuthenticationManager();
     }
 }
