071e422578e8504f5a0d1a3405abfa0f295abc53
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-07 19:22:14 +0530

    Implement JWT-based authentication in SecurityConfig, enhancing security configuration with stateless session management and custom authentication entry point. Update JwtAuthEntryPoint to be a Spring component for improved error handling in authentication processes.

--- src/main/java/com/shubh/jobportal/config/SecurityConfig.java
+++ src/main/java/com/shubh/jobportal/config/SecurityConfig.java
@@ -3,32 +3,63 @@ package com.shubh.jobportal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.shubh.jobportal.security.jwt.JwtAuthEntryPoint;
+import com.shubh.jobportal.security.jwt.JwtFilterChain;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthEntryPoint entryPoint;
+    private final JwtFilterChain filterChain;
+    private final UserDetailsService userDetailsService;
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-       return http
-       .csrf(CsrfConfigurer::disable)
-       .authorizeHttpRequests(
-        auth -> auth.requestMatchers("/**")
-        .permitAll()).build();
+    DaoAuthenticationProvider authenticationProvider() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(CsrfConfigurer::disable)
+                .authenticationProvider(authenticationProvider())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(entryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .addFilterBefore(filterChain, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception{
+    AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
         return builder.getAuthenticationManager();
     }
 }
