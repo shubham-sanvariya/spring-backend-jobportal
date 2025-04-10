e5bd8d975db731e504e28c8a00c22daf5a0c4855
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-10 16:28:45 +0530

    Remove StringOrLongValue interface as it is no longer needed following the refactor of JwtHelper, simplifying the codebase and improving maintainability.

--- src/main/java/com/shubh/jobportal/records/StringOrLongValue.java
+++
@@ -1,6 +0,0 @@
-package com.shubh.jobportal.records;
-
-public sealed interface StringOrLongValue permits StringOrLongValue.StringValue, StringOrLongValue.LongValue {
-    record StringValue(String value) implements StringOrLongValue {}
-    record LongValue(Long value) implements StringOrLongValue {}
-}
