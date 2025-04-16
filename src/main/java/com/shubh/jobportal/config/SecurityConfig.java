413cc72c1e4919d738fcbd2a9261de890a1644f9
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-16 21:11:12 +0530

    updated CORS configuration to allow requests from the production job portal URL

--- src/main/java/com/shubh/jobportal/config/SecurityConfig.java
+++ src/main/java/com/shubh/jobportal/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://react-job-portal-42b67z8sh-shubham-sanvariyas-projects.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
