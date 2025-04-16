005f07c1c8da1b8080525e2e2358dc95ff1c2661
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-16 12:26:21 +0530

    adding size validation on user dto

--- src/main/java/com/shubh/jobportal/dto/UserDTO.java
+++ src/main/java/com/shubh/jobportal/dto/UserDTO.java
@@ -5,6 +5,7 @@ import com.shubh.jobportal.entity.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,16 +15,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserDTO {
     private Long id;
+
     @NotBlank(message = "{user.name.absent}")
+    @Size(min = 3,max = 60)
     private String name;
+
     @NotBlank(message = "{user.email.absent}")
     @Email(message = "{user.email.invalid}")
     private String email;
+
     @NotBlank(message = "{user.password.absent}")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,15}$", message = "{user.password.invalid}")
     private String password;
 
+    @NotBlank
     private AccountType accountType;
+    
     private Long profileId;
 
     public User toEntity() {
