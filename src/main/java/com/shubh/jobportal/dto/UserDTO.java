16eec2f1da756a7c10adb1eeeffa83b9ef8527a4
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-16 20:52:14 +0530

    removed unused NotNull validation import from UserDTO

--- src/main/java/com/shubh/jobportal/dto/UserDTO.java
+++ src/main/java/com/shubh/jobportal/dto/UserDTO.java
@@ -4,7 +4,6 @@ import com.shubh.jobportal.entity.User;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
